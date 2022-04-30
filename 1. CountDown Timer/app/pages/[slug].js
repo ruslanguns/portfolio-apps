7e5cdec102e8c214b79769e76e0dbda414fc199f
Ruslan Gonzalez <ruslanguns@gmail.com>
2022-04-30 09:39:02 +0100

    Add event detail component

---
+++ 1. CountDown Timer/app/pages/[slug].js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import styles from '../styles/Event.module.css'
+import BackIcon from '../components/icons/BackIcon'
+import Heading from '../components/Heading'
+
+export default function Home() {
+  const router = useRouter()
+  const { slug } = router.query
+  const [isEdit, setIsEdit] = useState(true)
+
+  useEffect(() => {
+    slug && slug === 'edit' && setIsEdit(true)
+  }, [slug])
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{isEdit ? `Edit event | ${slug}` : 'Add an event'}</title>
+        <meta name="description" content="Create an event form" />
+      </Head>
+
+      <main className={styles.main}>
+        <Heading icon={BackIcon} title="Submit an event" />
+
+        <p className={styles.description}>Under construction!</p>
+      </main>
+    </div>
+  )
+}
