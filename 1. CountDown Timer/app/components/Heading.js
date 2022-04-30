0b412501244f95c43582a38c0b7b4f945b7e8eb9
Ruslan Gonzalez <ruslanguns@gmail.com>
2022-04-30 10:10:28 +0100

    Styling and responsiveness

--- 1. CountDown Timer/app/components/Heading.js
+++ 1. CountDown Timer/app/components/Heading.js
@@ -1,8 +1,17 @@
+import { useRouter } from 'next/router'
+
 const Heading = ({ icon: Icon, title }) => {
+  const router = useRouter()
+
   return (
-    <div className="relative flex flex-row gap-16">
-      <Icon className="absolute inline h-16 -left-24 w-14" />
-      <h1 className="inline mb-5 text-5xl font-normal ">{title}</h1>
+    <div className="relative flex flex-row   items-center leading-[2.5rem]  sm:h-14 ">
+      <Icon
+        onClick={() => router.push('/')}
+        className="left-0 w-9 h-9 sm:absolute sm:inline sm:-left-24 sm:w-14 sm:h-14 hover:cursor-pointer"
+      />
+      <h1 className="inline text-2xl font-normal align-middle sm:text-4xl">
+        {title}
+      </h1>
     </div>
   )
 }
