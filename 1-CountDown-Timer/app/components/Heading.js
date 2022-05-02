5a495433455d6b624f03df2ae8835349e02da458
Ruslan Gonzalez <ruslanguns@gmail.com>
2022-05-02 01:15:21 +0100

    Make back button icon bigger

--- 1-CountDown-Timer/app/components/Heading.js
+++ 1-CountDown-Timer/app/components/Heading.js
@@ -6,13 +6,13 @@ const Heading = ({ icon: Icon, title, ...props }) => {
 
   return (
     <div
-      className="relative flex flex-row items-center leading-[2.5rem] sm:h-14"
+      className="relative mb-6 flex flex-row items-center leading-[2.5rem] sm:h-14"
       {...props}
     >
       {Icon && (
         <Icon
           onClick={() => router.push('/')}
-          className="left-0 w-9 h-9 sm:absolute sm:inline sm:-left-24 sm:w-14 sm:h-14 hover:cursor-pointer"
+          className="left-0 w-12 h-12 mr-3 sm:absolute sm:inline sm:-left-24 sm:w-14 sm:h-14 hover:cursor-pointer"
         />
       )}
       <Title title={title} />
